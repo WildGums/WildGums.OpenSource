1b958a9a181611bd45c309360937129b6df782aa
Geert van Horrik <geert@catenalogic.com>
2021-02-02 10:30:47 +0100

    Script improvements

--- Scripts - generate site.cmd
+++ Scripts - generate site.cmd
@@ -1,6 +1,11 @@
 :: Copy theme from subrepository
-xcopy "repositories\hugo-theme-docdock" "src\themes\docdock" /e /y /i
-xcopy "src\themes\docdock.customize" "src\themes\docdock" /e /y /i
+for %%i in (docdock,doks) do (call:copyTheme %%i)
+
+:: Generate all sites
+:: for %%i in (home,5.0,5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,5.10,5.11,5.12,vnext) do (call:buildSite %%i)
+call:buildSite main
+
+goto:eof
 
 :buildSite
 set siteName=%~1
@@ -10,4 +15,15 @@ echo Building site: %siteName%
 :: Build site
 cd .\src
 hugo
-pause
+goto:eof
+
+:copyTheme
+set themeName=%~1
+
+echo Copying theme: %themeName%
+
+:: Copy shared stuff
+xcopy "repositories\hugo-theme-%themeName%" "src\shared\themes\%themeName%" /e /y /i
+xcopy "src\themes\%themeName%.customize" "src\shared\themes\%themeName%" /e /y /i
+
+goto:eof
